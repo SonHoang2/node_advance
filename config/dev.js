3cdbe233a0b81eaf969a0de594d82eadec5289da
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2024-05-25 13:11:46 +0700

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: "739394782224-27atvp5qpn5mjmjdtpnk2t81mtnujus6.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb://localhost:27017",
+  cookieKey: "123123123",
+};
